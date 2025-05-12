f7773792412da3140837ffb6746a0f79806e84ca
shiwani08 <shiwanisoni29082002@gmail.com>
2025-05-12 12:56:28 +0530

    image created

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,9 @@
+# 1.  build the docker image: docker build -t myapp .
+# 2. then see the images: docker images 
+#  or: docker image ls
+# 3. run the image: docker run myapp
+
 FROM node:alpine 
+COPY . /app
+WORKDIR /app
+CMD node app.js
--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log("Dockering ")
+
+const fs = require('fs');
+
+fs.stat('app.js', (err, stats) => {
+    if (err) {
+        console.log(`File doesn't exist.`);
+    } else {
+        console.log(`File size is:`, stats.size, "MB"); // File size in bytes
+    }
+});
+
+// as of now, the file size of js is 226 MB, but when i use docker, the file size is 165 MB
+// this is jsut a speculation, does not happen always\
+// the size of the file generally increase, but alpine may reduce save space
